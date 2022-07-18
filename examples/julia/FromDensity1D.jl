5e19d3b76d4e40915144e5b50a3e614e1693b3ee
Danny Sharp <dannys4@mit.edu>
2022-07-18 16:09:00 -0400

    working commit

--- examples/julia/FromDensity1D.jl
+++ examples/julia/FromDensity1D.jl
@@ -1,14 +1,18 @@
-include("MParT.jl")
+include("/home/dannys4/installs/MPART/julia/mpart/MParT.jl")
 using .MParT, CxxWrap
 using Distributions, Optimization, OptimizationOptimJL
 
-make_plot = false
+make_plot = true
+
+if make_plot
+    using CairoMakie
+end
 ##
 
 num_points = 5000
 mu = 2
 sigma = 0.5
-x = randn(num_points)
+x = randn(1,num_points)
 
 rv = Normal(mu, sigma)
 t = range(-3,6,length=100)
@@ -17,7 +21,11 @@ rho_t = pdf.(rv, t)
 num_bins = 50
 fig = nothing
 if make_plot
-
+    fig = Figure()
+    ax1 = Axis(fig[1,1], title="Before Optimization")
+    hist!(ax1, x[:], bins=num_bins, color=(:blue,0.5), normalization=:pdf, label="ReferenceSamples")
+    scatter!(ax1, t, rho_t, color=:red, label="Target Density")
+    axislegend(ax1)
 end
 fig
 
@@ -29,15 +37,34 @@ opts = MapOptions()
 monotoneMap = CreateComponent(fixed_mset, opts)
 
 # KL divergence objective
-function objective(coeffs)
-    SetCoeffs(to_base(monotoneMap), coeffs)
-    map_of_x = Evaluate(to_base(monotoneMap), x)
+function objective(coeffs,_)
+    SetCoeffs(monotoneMap, coeffs)
+    map_of_x = Evaluate(monotoneMap, x)
     pi_of_map_of_x = logpdf.(rv, map_of_x)
     log_det = LogDeterminant(monotoneMap, x)
-    -sum(pi_of_map_of_x + log_det)/num_points
+    -sum(vec(pi_of_map_of_x) + log_det)/num_points
 end
+
+prob = OptimizationProblem(objective, [0.,0.], nothing)
 
 # Optimize
 println("Starting Coeffs")
-println(CoeffMap(to_base(monotoneMap)))
-println("And error $(objective(CoeffMap(to_base(monotoneMap))))")
+u0 = CoeffMap(monotoneMap)
+println(u0)
+println("And error $(objective(u0,nothing))")
+sol = solve(prob, NelderMead())
+u_final = sol.u
+SetCoeffs(monotoneMap, u_final)
+println("Final Coeffs")
+println(u_final)
+println("And error $(objective(u_final,nothing))")
+
+## After optimization Plot
+map_of_x = Evaluate(monotoneMap, x)
+if make_plot
+    ax2 = Axis(fig[2,1], title="After Optimization")
+    hist!(ax2, map_of_x[:], bins=num_bins, color=(:blue,0.5), normalization=:pdf, label="OptimizedSamples")
+    scatter!(ax2, t, rho_t, color=:red, label="Target Density")
+    axislegend(ax2)
+end
+fig
