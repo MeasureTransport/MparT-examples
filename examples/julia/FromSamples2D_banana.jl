2979e85ed35cf14610c58f2820898fcb673a3645
Danny Sharp <dannys4@mit.edu>
2022-07-18 23:50:08 -0400

    Working banana

--- examples/julia/FromSamples2D_banana.jl
+++ examples/julia/FromSamples2D_banana.jl
@@ -1,7 +1,7 @@
 include("MParT.jl")
 ##
 using .MParT, CxxWrap
-using Distributions, LinearAlgebra, Optimization, OptimizationOptimJL
+using Distributions, LinearAlgebra, Statistics, Optimization, OptimizationOptimJL
 
 make_plot = true
 
@@ -13,18 +13,81 @@ num_points = 1000
 z = randn(2,num_points)
 x1 = z[1,:]
 x2 = z[1,:] + z[2,:].^2
-x = [x1; x2]
+x = collect([x1 x2]')
 
 test_n_pts = 10_000
 test_z = randn(2,test_n_pts)
 test_x1 = test_z[1,:]
 test_x2 = test_z[1,:] + test_z[2,:].^2
-test_x = [test_x1; test_x2]
+test_x = collect([test_x1 test_x2]')
 
 # For Plotting and computing reference density
 rho = MvNormal(I(2))
 t = range(-5,5,length=100)
-grid = [[t1,t2] for t1 in t, t2 in t]
-rho_t = pdf.(rv, grid)
+rho_t = [pdf(rho, [t1,t2]) for t1 in t, t2 in t]
 
 ## Set up map and initialize coefficients
+opts = MapOptions()
+tri_map = MParT.CreateTriangular(2,2,2,opts)
+coeffs = zeros(MParT.numCoeffs(tri_map))
+
+function obj(coeffs, nothing)
+    SetCoeffs(tri_map, coeffs)
+    map_of_x = Evaluate(tri_map, x)
+    rho_of_map_of_x = logpdf(rho, map_of_x)
+    log_det = LogDeterminant(tri_map, x)
+    -sum(rho_of_map_of_x + log_det)/num_points
+end
+
+function grad_obj(g, coeffs, nothing)
+    SetCoeffs(tri_map, coeffs)
+    map_of_x = Evaluate(tri_map, x)
+    grad_rho_of_map_of_x = -CoeffGrad(tri_map, x, map_of_x)
+    grad_log_det = LogDeterminantCoeffGrad(tri_map, x)
+    g .= -sum(grad_rho_of_map_of_x + grad_log_det, dims=2)/num_points
+end
+
+## Plot before Optimization
+map_of_x = Evaluate(tri_map, x)
+fig = nothing
+if make_plot
+    fig = Figure()
+    ax1 = Axis(fig[1,1], title="Before Optimization")
+    contour!(ax1, t, t, rho_t)
+    scatter!(ax1, test_x[1,:], test_x[2,:], color=(:blue,0.5), label="Target Samples")
+    axislegend(ax1)
+end
+
+u0 = CoeffMap(tri_map)
+fcn = OptimizationFunction(obj, grad = grad_obj)
+prob = OptimizationProblem(fcn, u0, nothing, g_tol = 1e-16)
+
+## Optimize
+
+
+println("Starting coeffs")
+println(u0)
+println("and error: $(obj(u0,nothing))")
+println("===================")
+@time(sol = solve(prob, BFGS()))
+##
+u_final = sol.u
+SetCoeffs(tri_map, u_final)
+println("Final coeffs")
+println(u_final)
+println("and error: $(obj(u_final,nothing))")
+println("===================")
+map_of_test_x = Evaluate(tri_map, test_x)
+if make_plot
+    ax2 = Axis(fig[2,1], title="After Optimization")
+    contour!(ax2, t, t, rho_t)
+    scatter!(ax2, map_of_test_x[1,:], map_of_test_x[2,:], color=(:blue,0.5), label="Target Samples")
+    axislegend(ax2)
+end
+mean_of_map = mean(map_of_test_x, dims=2)
+cov_of_map = cov(map_of_test_x, dims=2)
+println("Mean of map")
+println(mean_of_map)
+println("Covariance of map")
+println(cov_of_map)
+fig
