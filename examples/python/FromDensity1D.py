a18e75be6f14ae2086c5b1fa66224386520c3f22
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2022-08-15 16:39:45 -0400

    a little bit of text and a cdf figure

--- examples/python/FromDensity1D.py
+++ examples/python/FromDensity1D.py
@@ -14,28 +14,22 @@
 #     name: python3
 # ---
 
-# ### Construct map from density
-#
-# A way to construct a transport map is from an unnormalized density. 
-
-# First, import MParT and other packages used in this notebook. Note that it is possible to specify the number of threads used by MParT by setting the `KOKKOS_NUM_THREADS` environment variable before importing MParT.
-
-# +
-
 # # Construct map from density
 #
 # One way to construct a transport map is from an unnormalized density. 
 
->>>>>>> origin/main
+# First, import MParT and other packages used in this notebook. Note that it is possible to specify the number of threads used by MParT by setting the `KOKKOS_NUM_THREADS` environment variable before importing MParT.
+
+# +
 import math
 import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-from mpart import *
+import mpart
 
-print('Kokkos is using', Concurrency(), 'threads')
+print('Kokkos is using', mpart.Concurrency(), 'threads')
 # -
 # The target distribution is given by $x\sim\mathcal{N}(2, 0.5)$.
 
@@ -50,26 +44,28 @@ reference_density = norm(loc=mu,scale=sigma)
 t = np.linspace(-3,6,100)
 rho_t = reference_density.pdf(t)
 
-num_bins = 50
-# Before optimization plot
 plt.figure()
-plt.hist(x.flatten(), num_bins, facecolor='blue', alpha=0.5, density=True, label='Reference samples')
+plt.hist(x.flatten(), bins="auto", facecolor='blue', alpha=0.5, density=True, label='Reference samples')
 plt.plot(t, rho_t,label="Target density")
+plt.yticks([])
 plt.legend()
+plt.title("Before optimization")
 plt.show()
 
-# Next we create a multi-index set and create a map.
+# Next we create a multi-index set and create a map. Affine transform should be enough to capture the Gaussian target.
 
-multis = np.array([[0], [1]])  # affine transform enough to capture Gaussian target
-mset = MultiIndexSet(multis)
+multis = np.array([[0], [1]])  # 
+mset = mpart.MultiIndexSet(multis)
 fixed_mset = mset.fix(True)
 
-# Set MapOptions and make map
-opts = MapOptions()
-monotoneMap = CreateComponent(fixed_mset, opts)
+# Now we set the map options (default in this case) and initialize the map
 
-# KL divergence objective
+opts = mpart.MapOptions()
+monotoneMap = mpart.CreateComponent(fixed_mset, opts)
 
+# Next we optimize the coefficients of the map by minimizing the Kullback–Leibler divergence between the target and reference density.
+
+# +
 def objective(coeffs, monotoneMap, x, rv):
     num_points = x.shape[0]
     monotoneMap.SetCoeffs(coeffs)
@@ -78,7 +74,16 @@ def objective(coeffs, monotoneMap, x, rv):
     log_det = monotoneMap.LogDeterminant(x)
     return -np.sum(pi_of_map_of_x + log_det)/num_points
 
-# Optimize
+def obj(coeffs, transport_map, x):
+    transport_map.SetCoeffs(coeffs)
+    map_of_x = transport_map.Evaluate(x)
+    logpdf= target_logpdf(map_of_x)
+    log_det = transport_map.LogDeterminant(x)
+    return -np.sum(logpdf + log_det)/num_points
+
+
+# -
+
 print('Starting coeffs')
 print(monotoneMap.CoeffMap())
 print('and error: {:.2E}'.format(objective(monotoneMap.CoeffMap(), monotoneMap, x, reference_density)))
@@ -87,10 +92,23 @@ print('Final coeffs')
 print(monotoneMap.CoeffMap())
 print('and error: {:.2E}'.format(objective(monotoneMap.CoeffMap(), monotoneMap, x, reference_density)))
 
-# After optimization plot
+# ...and plot the results. 
+
+# +
 map_of_x = monotoneMap.Evaluate(x)
-plt.figure()
-plt.hist(map_of_x.flatten(), num_bins, facecolor='blue', alpha=0.5, density=True, label='Mapped samples')
-plt.plot(t,rho_t,label="Target density")
-plt.legend()
+fig, axs = plt.subplots(2,1)
+axs[0].hist(x.flatten(), bins="auto", alpha=0.5, density=True, label='Reference samples')
+axs[0].hist(map_of_x.flatten(), bins="auto", facecolor='blue', alpha=0.5, density=True, label='Mapped samples')
+axs[0].plot(t,rho_t,label="Target density")
+axs[0].legend()
+axs[0].set_yticks([])
+
+axs[1].hist(x.flatten(), bins="auto", density=True, label='Reference samples', cumulative=True, histtype='step')
+axs[1].hist(map_of_x.flatten(), bins="auto", density=True, label='Mapped samples', cumulative=True, histtype='step')
+axs[1].plot(t, reference_density.cdf(t), label='Target density')
+axs[1].legend()
+axs[1].set_yticks([])
+
+fig.suptitle("After optimization")
+
 plt.show()
