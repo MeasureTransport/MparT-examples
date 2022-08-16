6950cad61e8f77ea62b3ee60a48039ee2c0c5c8b
rubiop <paulbaptiste.rubio@gmail.com>
2022-08-16 18:25:30 -0400

    added kernel spec

--- examples/python/FromDensity2D_banana.py
+++ examples/python/FromDensity2D_banana.py
@@ -6,6 +6,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 # + [markdown] id="u_tcbBTTACPG"
