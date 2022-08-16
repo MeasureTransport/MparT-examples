0392eae358557331c010917f4e106fa51a3ffacb
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2022-08-16 10:41:20 -0400

    readded kernel specs

--- examples/python/MonotoneLeastSquares.py
+++ examples/python/MonotoneLeastSquares.py
@@ -7,6 +7,10 @@
 #       format_name: light
 #       format_version: '1.5'
 #       jupytext_version: 1.14.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
 # ---
 
 # + [markdown] id="C2Nh9W00oG1R"
