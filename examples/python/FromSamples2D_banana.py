83c4bd8912db4c5328d5c550fb21511c478e796b
rubiop <paulbaptiste.rubio@gmail.com>
2022-08-17 11:53:43 -0400

    changed kernelspec

--- examples/python/FromSamples2D_banana.py
+++ examples/python/FromSamples2D_banana.py
@@ -8,7 +8,7 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.14.0
 #   kernelspec:
-#     display_name: Python 3.8.5 ('base')
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
