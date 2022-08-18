8805cdb44b3b8a195be9f51b7c9e83d530429024
rubiop <paulbaptiste.rubio@gmail.com>
2022-08-17 20:39:24 -0400

    added .mlx to gitignore

---
+++ examples/matlab/MonotoneLeastSquares.m
@@ -0,0 +1,24 @@
+%% 
+% To open this file as live script jfrom the Editor, right-click the document 
+% tab, and select *Open MonotoneLeastSquares.mlx as Live Script* from the context 
+% menu. Alternatively, go to the *Editor* tab, click *Save*, and select *Save 
+% As*. Then, set the *Save as type:* to |MATLAB Live Code Files (*.mlx)| and click 
+% *Save*.
+%% Montone least squares
+% This example is nice $T$
+% 
+% 
+% 
+% 
+% 
+% 
+
+clear;
+addpath(genpath('~/Installations/MParT/matlab/')) %installation path
+
+%% 
+% Salut
+% 
+% 
+
+matlab.internal.liveeditor.openAndConvert('MonotoneLeastSquares.mlx','MonotoneLeastSquares.m')
