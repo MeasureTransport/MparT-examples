8eda8fc3749391ad6deadc633d04d737694e19eb
rubiop <paulbaptiste.rubio@gmail.com>
2023-03-22 09:00:32 +0100

    debug atm training

--- examples/python/SequentialBayesianInference.py
+++ examples/python/SequentialBayesianInference.py
@@ -58,9 +58,12 @@ plt.show()
 mu0 = 2;
 std0 = 0.25;
 
+# Measurement noise
+std_m = 63
+
 N=20000; # total number of samples
 X = mu0 + std0*np.random.randn(1,N);
-Y = sigma_eff(sigma_w,sigma_i,X);
+Y = sigma_eff(sigma_w,sigma_i,X)+std_m*np.random.randn(1,N);
 
 samples = np.vstack((X,Y))
 
@@ -70,16 +73,49 @@ samples_train = samples[:,:n_train]
 samples_test = samples[:,n_train:]
 # -
 
+# plot samples
+fig,ax = plt.subplots()
+ax.plot(samples_train[0,:],samples_train[1,:],'r.')
+plt.show()
+
+# +
+# Standardization of samples
+C = np.cov(samples_train);
+A = np.linalg.inv(np.linalg.cholesky(C));
+c = -1 * np.dot(A,np.mean(samples_train,axis=1));
+
+L = mt.AffineMap(np.asfortranarray(A),c)
+
+samples_train_st = L.Evaluate(samples_train)
+samples_test_st = L.Evaluate(samples_test)
+# -
+
+# Plot standardized samples
+fig,ax = plt.subplots()
+ax.plot(samples_train_st[0,:],samples_train_st[1,:],'g.')
+plt.show()
+
 atm_opts=mt.ATMOptions()
 atm_opts.basisLB = -3
 atm_opts.basisUB = 3
 atm_opts.verbose = 1
+atm_opts.maxSize = 15
 
 msets = [mt.MultiIndexSet.CreateTotalOrder(2,1)]
-obj = mt.CreateGaussianKLObjective(np.asfortranarray(samples_train),np.asfortranarray(samples_test),1)
-atm_map = mt.AdaptiveTransportMap(msets,obj,atm_opts)
+obj = mt.CreateGaussianKLObjective(np.asfortranarray(samples_train_st),np.asfortranarray(samples_test_st),1)
+[mset,atm_map] = mt.AdaptiveTransportMap(msets,obj,atm_opts)
 
-train_options = mpart.TrainOptions()
-mt.TrainMap()
+# +
+from scipy.stats import norm
+
+X_std = atm_map.Evaluate(samples_test)
+fig,ax = plt.subplots()
+ax.hist(X_std, density=True, bins=30, alpha=0.5, label='Sample Histogram')
+x_axis = np.linspace(-4, 4, 100)
+pdf = norm.pdf(x_axis)
+plt.plot(x_axis, pdf, color='r', label='Normal PDF')
+# -
+
+X_std.shape
 
 
