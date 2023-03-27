f3294ba9da95f9b5bc349af93d3f00da4dfe28e5
rubiop <paulbaptiste.rubio@gmail.com>
2023-03-27 16:07:28 +0200

    Compute gradient

--- examples/matlab/SequentialBayesianInference.m
+++ examples/matlab/SequentialBayesianInference.m
@@ -1,7 +1,10 @@
 clear
 close all
 
+rng(4)
+
 addpath(genpath('~/Installations/MParT/matlab/'))
+addpath(genpath('~/Documents/code/ATM/adaptivetransportmaps'))
 KokkosInitialize(8)
 
 sigma_w=2500;
@@ -46,7 +49,7 @@ atm_opts.basisLB = -3;
 atm_opts.basisUB = 3;
 atm_opts.verbose = 1;
 atm_opts.maxSize = 20;
-atm_opts.maxDegrees = MultiIndex([8,8]);
+atm_opts.maxDegrees = MultiIndex([10,10]);
 
 msets = [MultiIndexSet.CreateTotalOrder(1,1),MultiIndexSet.CreateTotalOrder(2,1)];
 
@@ -63,17 +66,75 @@ X = LS.Evaluate(samples_train);
 figure
 hold on
 plot(X(1,:),X(2,:),'.b')
-
+axis equal
 
 %% Observations
-
-
 N=40;
 List_obs=sigma_eff(sigma_w,sigma_i,2)+randn(N,1);
+
+
+ref2 = IndependentProductDistribution(repmat({Normal()},1,2));
+CM=load('offline_map_2_25.mat').CM;
+L_M={CM.S{1}.S CM.S{2}.S};
+yobs=List_obs(1);
+PB_off=ComposedPullbackDensity(L_M,ref2);
+
+lkl = LikelihoodFunction(PB_off, yobs);
+
+xlin = linspace(1,3,100);
+
+x_jt=[xlin', repmat(yobs,100,1)];
+
+y = PB_off.evaluate(x_jt);
+y2 = LS.Evaluate(x_jt');
+
+
+Gy=LS.Gradient(x_jt',[zeros(1,100);ones(1,100)]);
+
+
+
+DY = gradient(Y(1,:),X(1,:));
+
+
+figure
+plot(xlin,y(:,2))
+hold on
+plot(xlin,y2(2,:))
+title('S(X)')
+
+figure
+plot(xlin,lkl.log(xlin'))
+title('Log-likelihood')
 
 
 
 function [out] = sigma_eff(sigma_w,sigma_i,z_iw)
     Rz_iw=1./sqrt(4*z_iw.^2+1);
     out=sigma_i.*(1-Rz_iw)+sigma_w.*(Rz_iw);
+end
+
+function [log] = LikelihoodFunction_M(S,yobs,X)   
+    
+    dobs = length(yobs);
+    dtheta = S.outputDim - dobs;
+    x_jt=[X;repmat(yobs,1,size(X,1))];
+    
+    SX = S.Evaluate(x_jt);
+    Gy=LS.Gradient(x_jt,[zeros(dtheta,100);ones(dobs,100)]);
+    log=normlogpdf(SX(end-dobs:end,:));
+end
+
+function log_pdf=log_cond_pullback_pdf(triMap,x)
+%log-conditonal pullback density
+r = triMap.Evaluate(x);
+log_pdf = normlogpdf(r)+triMap.LogDeterminant(x)';
+end
+
+function logpdf=normlogpdf(varargin)
+    x = varargin{1};
+    if nargin ==1
+    logpdf = -0.5*(log(2*pi)+x.^2);
+    else
+     logpdf = -log(sqrt(2*pi)*varargin{3})-0.5*((x-varargin{2})/varargin{3}).^2;
+    end
 end
